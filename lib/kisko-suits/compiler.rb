d162a809a75d33a074eb9101fbcf4bc1d84f84b6
Vesa Vänskä <vesa@vesavanska.com>
2015-05-14 23:03:44 +0300

    Adds implementation for including from includes

--- lib/kisko-suits/compiler.rb
+++ lib/kisko-suits/compiler.rb
@@ -2,10 +2,10 @@ require "set"
 
 module KiskoSuits
   class Compiler
-    attr_reader :file, :output_filename, :included_filenames
+    attr_reader :path, :output_filename, :included_filenames
 
-    def initialize(filename)
-      @filename = filename
+    def initialize(path)
+      @path = path
       @output_filename = nil
 
       @included_filenames = Set.new
@@ -14,25 +14,26 @@ module KiskoSuits
     def render
       @included_filenames.clear
 
-      abort "Suits file '#{@filename}' not found" unless File.exists?(@filename)
+      abort "Suits file '#{path}' not found" unless File.exists?(path)
 
       open_output_file do |output|
-        File.foreach(@filename) do |line|
-          output.write(process_line(line))
+        File.foreach(path) do |line|
+          output.write(process_line(File.dirname(path), line))
         end
       end
     end
 
     private
 
-    def process_line(line)
-      root_dir = File.dirname(@filename)
-
+    def process_line(root_dir, line)
       if match = line.match(/\s*include:\s*([\w\.\/]+)/)
         included_path = "#{root_dir}/#{match[1]}"
         if File.exists?(included_path)
           @included_filenames << included_path
-          File.read(included_path)
+
+          File.foreach(included_path).map { |included_line|
+            process_line(File.dirname(included_path), included_line)
+          }.join
         else
           puts "Include #{included_path} can't be found"
           ""
@@ -43,9 +44,9 @@ module KiskoSuits
     end
 
     def open_output_file(&block)
-      @output_filename = @filename.gsub(".suits", "")
+      @output_filename = path.gsub(".suits", "")
 
-      abort "Problem with config file (should end with .suits)" if @filename == @output_filename
+      abort "Problem with config file (should end with .suits)" if path == @output_filename
 
       File.delete(@output_filename) if File.exists?(@output_filename)
       File.open(@output_filename, 'w', &block)

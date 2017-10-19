b1cb7304202262aec5e9376c9a8be83d02494d5d
Vesa Vänskä <vesa@vesavanska.com>
2017-10-20 00:10:50 +0300

    Adds tests for variables

--- spec/kisko-suits/compiler_spec.rb
+++ spec/kisko-suits/compiler_spec.rb
@@ -4,17 +4,34 @@ describe KiskoSuits::Compiler do
   subject { KiskoSuits::Compiler.new(filename) }
 
   describe "#render" do
-    let(:filename) { "data/preso.md.suits" }
+    context "with suits file" do
+      let(:filename) { "data/preso.md.suits" }
 
-    it "creates output file" do
-      subject.render
-      output = File.read("data/preso.md")
-      expected_output = File.read("data/preso.md_proper")
-      expect(output).to eq(expected_output)
+      it "creates output file" do
+        subject.render
+        output = File.read("data/preso.md")
+        expected_output = File.read("data/preso.md_proper")
+        expect(output).to eq(expected_output)
+      end
+
+      after do
+        File.delete("data/preso.md")
+      end
     end
 
-    after do
-      File.delete("data/preso.md")
+    context "with suits file with variables" do
+      let(:filename) { "data/preso_with_variables.md.suits" }
+
+      it "creates output file" do
+        subject.render
+        output = File.read("data/preso_with_variables.md")
+        expected_output = File.read("data/preso_with_variables.md_proper")
+        expect(output).to eq(expected_output)
+      end
+
+      after do
+        File.delete("data/preso_with_variables.md")
+      end
     end
   end
 end
